126e2f6db373df5514228e65eca2d686879dd9b2
𝐀𝐑𝐒𝐇`𝐗´ 😈 <149147034+asrshx@users.noreply.github.com>
2026-05-06 04:16:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,7 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-# ALL-IN-ONE COMPACT CARD THEME
- custom_css = 
-"""
+ custom_css = """
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap');
 
